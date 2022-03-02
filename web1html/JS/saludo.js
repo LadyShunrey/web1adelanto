4455a7732dcd4aae3f1f86d5f21eb67e328f045a
Melisa Burlando <melisa.burlando@gmail.com>
2022-03-02 16:51:40 -0300

    agrego el js en web1 y agrego divs en el tp1

---
+++ web1html/JS/saludo.js
@@ -0,0 +1,24 @@
+console.log("empieza el programa");
+
+let nombre = prompt("Ingrese su nombre por favor");
+console.log( nombre );
+alert(nombre);
+
+alert("Hola Mundo!");
+let saludo = "hola mundo! en variable!";
+console.log(saludo);
+
+alert("Hola de nuevo!!");
+
+console.log("segundo mensajee");
+
+alert("tercer mensaje, jajaja bueno puede ser medio troll xD");
+
+/* cons es una constante */
+
+let edad = prompt("ingrese su edad por favor");
+alert(edad);
+console.log (edad);
+
+let variable = prompt("Pregunta???");
+
