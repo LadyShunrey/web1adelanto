eabce8bf5160889839db2963fb4f10984fc6773c
Melisa Burlando <melisa.burlando@gmail.com>
2022-03-29 13:13:28 -0300

    edito los nombres de las carpetas para que esten todos en minusculas

--- web1html/JS/lampara.js
+++ web1html/JS/lampara.js
@@ -4,14 +4,14 @@ apagar();
 
 function prender(){
     //cambiar la foto
-    document.getElementById("imagen-lampara").src="IMAGES/prendida.png";
+    document.getElementById("imagen-lampara").src="images/prendida.png";
     //cambiar el fondo
     document.getElementById("fondo").classList.add("prendido");
     document.getElementById("fondo").classList.remove("apagado");
 }
 
 function apagar(){
-    document.getElementById("imagen-lampara").src="IMAGES/apagada.png";
+    document.getElementById("imagen-lampara").src="images/apagada.png";
     document.getElementById("fondo").classList.add("apagado");
     document.getElementById("fondo").classList.remove("prendido");
 }
