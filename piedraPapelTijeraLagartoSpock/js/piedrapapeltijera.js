66a1faa101841cce30a060023c863a82d6467604
Melisa Burlando <90287257+LadyShunrey@users.noreply.github.com>
2022-06-05 19:18:15 -0300

    trabajando en el piedra papel tijera lagarto spock

--- piedraPapelTijeraLagartoSpock/js/piedrapapeltijera.js
+++ piedraPapelTijeraLagartoSpock/js/piedrapapeltijera.js
@@ -4,19 +4,39 @@
 let boton = document.querySelector("#boton-play");
 boton.addEventListener('click', jugar);
 
+
+
+
 //declaro el arreglo de opciones de la máquina
 let opciones = ["piedra", "papel", "tijera", "lagarto", "Spock"];
 
 function jugar (){
-    console.log("estoy dentro de la funcion");
-    console.log(opciones);
+    console.log("estoy dentro de la funcion jugar");
+    console.log("el tamano es " + opciones.length);
+    console.log("las opciones son " + opciones);
 
     //genero un numero random entre 0 y 4
-    let random = Math.floor(Math.random() * 5);
+    let random = Math.floor(Math.random() * opciones.length);
     console.log(random);
     let opcion = opciones[random];
 
     //escribo la opcion en el html
     document.querySelector('.result').innerHTML = opcion;
 
+    //cambio la imagen
+    let result = document.querySelector('#result');
+
+    result.src = "images/" + random + ".png";
+}
+
+
+let botonElegir = document.querySelector("#boton-elegir");
+botonElegir.addEventListener('click', cambiarImagen)
+
+function cambiarImagen{
+    console.log("estoy dentro de la funcion cambiar imagen");
+    let choice = document.querySelector('#choice');
+    console.log(choice);
+    //laEleccion valor;
+    choice.src="images/" + laEleccion + ".png";
 }
