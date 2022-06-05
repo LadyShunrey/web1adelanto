68d9e6949fa77de85f6cf0dafa5f2e80c711f7b5
Melisa Burlando <90287257+LadyShunrey@users.noreply.github.com>
2022-06-05 14:50:10 -0300

    sigo desarrollando el piedra papel tijera lagarto spock

--- piedraPapelTijeraLagartoSpock/js/piedrapapeltijera.js
+++ piedraPapelTijeraLagartoSpock/js/piedrapapeltijera.js
@@ -10,10 +10,13 @@ let opciones = ["piedra", "papel", "tijera", "lagarto", "Spock"];
 function jugar (){
     console.log("estoy dentro de la funcion");
     console.log(opciones);
-    //genero un numero random entre cero y dos
-    let random = Math.floor(Math.random() * 3);
+
+    //genero un numero random entre 0 y 4
+    let random = Math.floor(Math.random() * 5);
     console.log(random);
     let opcion = opciones[random];
+
     //escribo la opcion en el html
     document.querySelector('.result').innerHTML = opcion;
+
 }
